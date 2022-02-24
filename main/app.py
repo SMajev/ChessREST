a9b79d7ae0426f6feb76c241bf54da11640f22d8
smajev <smajev@gmail.com>
2022-02-24 15:19:13 +0100

    bishop & knight

--- main/app.py
+++ main/app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
+@app.route('/test', methods=['GET'])
+def test():
+    return jsonify(result='To jest test')
+
+@app.route('/test2', methods=['POST'])
+def test2():
+    request_data = request.get_json()
+    return jsonify(result='To jest test')
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
--- main/figures.py
+++ main/figures.py
@@ -1,23 +1,5 @@
 from abc import ABC, abstractmethod
-
-
-class Figure(ABC):
-    @abstractmethod
-    def __init__(self, board, x, y):
-        self.board = board
-        self.name = ""
-        self.color = ""
-        self.y = y
-        self.x = x
-        self.position = self.board[self.y][self.x]
-
-    @abstractmethod
-    def list_available_moves(self):
-        pass
-
-    @abstractmethod
-    def validate_move(self, dest_field):
-        pass
+from figure import Figure
 
 
 class Pawn(Figure):
